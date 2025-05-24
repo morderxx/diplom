b5b61c262f0b7213fc0b0dcbbff7e8cc26bb5af7
morderxx <91130583+morderxx@users.noreply.github.com>
2025-05-24 20:51:45 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -66,7 +66,7 @@ app.use('/api/rooms', roomsRoutes);
 app.use('/api/files', filesRoutes);
 app.use(
   '/miniapps',
-  express.static(path.join(__dirname, 'server', 'miniapps'))
+  express.static(path.join(__dirname, 'miniapps'))
 );
 
 // 3) Статика клиентской части
