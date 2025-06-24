9fe420513186bd1cb50ce47300656bf99bd87da7
morderxx <91130583+morderxx@users.noreply.github.com>
2025-06-24 19:23:04 +0300

    Update profile.js

--- server/client/profile.js
+++ server/client/profile.js
@@ -7,7 +7,7 @@ async function saveProfile() {
     const birthdate = document.getElementById('birthdate').value;
     const bio       = document.getElementById('bio').value;
     const token     = localStorage.getItem('token');
-
+    const age = birthdate;
     // Проверка заполненности всех полей
     if (!nickname || !full_name || !birthdate || !bio) {
         document.getElementById('message').innerText = 'Все поля обязательны для заполнения';
@@ -17,15 +17,15 @@ async function saveProfile() {
     // Проверка возраста (минимум 12 лет)
     const birthDate = new Date(birthdate);
     const today = new Date();
-    let age = today.getFullYear() - birthDate.getFullYear();
+    let ageC = today.getFullYear() - birthDate.getFullYear();
     const monthDiff = today.getMonth() - birthDate.getMonth();
     
     // Корректировка возраста, если день рождения ещё не наступил
     if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
-        age--;
+        ageC--;
     }
     
-    if (age < 12) {
+    if (ageC < 12) {
         document.getElementById('message').innerText = 'Вы должны быть старше 12 лет';
         return;
     }
@@ -39,7 +39,7 @@ async function saveProfile() {
         body: JSON.stringify({
             nickname,
             full_name,
-            birthdate,  // Отправляем возраст в годах
+            age,  
             bio
         })
     });
